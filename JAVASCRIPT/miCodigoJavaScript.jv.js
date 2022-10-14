d8898d3271122faf84335baaa40fbd52339d34ce
txellcom <114602050+txellcom@users.noreply.github.com>
2022-10-14 18:10:37 +0200

    JS

---
+++ JAVASCRIPT/miCodigoJavaScript.jv.js
@@ -0,0 +1,5 @@
+function cambiaParrafo() {
+    //Esta acción es un código de JavaScript que cambia el párrafo del documento HTML
+    document.getElementById("cambia").innerHTML = "Párrafo cambiado a través del código JS externo";
+    
+}
